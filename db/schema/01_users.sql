abd296b0383e452adf7607f341354facc8128958
Rebecca Chen <rebecca061996@gmail.com>
2021-02-05 23:58:52 +0000

    create users table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,11 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate Users table
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  first_name VARCHAR(255) NOT NULL
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  is_deleted BOOLEAN
 );
