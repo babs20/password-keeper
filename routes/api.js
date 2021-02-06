f22de965708c9b2194b72387dad3dc64c877a828
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 15:59:26 +0000

    create route to get user information

--- routes/api.js
+++ routes/api.js
@@ -9,21 +9,6 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (database) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`; // update query
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  });
-
   router.get("/user", (req, res) => {
     const userId = req.body.id;
     database.getUserInfo(userId)
