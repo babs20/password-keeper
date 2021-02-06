ad10d3d21b6353cde63a8c6a534864d9fd1d7e14
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 16:36:16 +0000

    create function to insert new user into database

--- server/database.js
+++ server/database.js
@@ -9,24 +9,48 @@ const getUserWithEmail = function(email) {
 };
 exports.getUserWithEmail = getUserWithEmail;
 
+
+/**
+   * Add user to database with given information
+   * @param {Object} user
+**/
 const addUser = (user) => {
   const query = `
-  INSERT INTO organizations (name, abbreviation, email, password, identifier_key)
-  VALUES ($1, $2, $3, $3, $4)
+  INSERT INTO users (first_name, last_name, email, password)
+  VALUES ($1, $2, $3, $4)
   RETURNING *;`
-  return db.query(query, [user.name, user.abbr])
-    .then((res) => res.rows)
+  return db.query(query, [user.first_name, user.last_name, user.email, user.password])
+    .then((res) => res.rows[0])
     .catch();
 };
 exports.addUser = addUser;
 
+/**
+   * Add user to database with given information
+   * @param {String} userId
+**/
 const getUserInfo = (userId) => {
   const query = `
   SELECT *
   FROM users
   WHERE id =$1;`
   return db.query(query, [userId])
-    .then((res) => res.rows)
+    .then((res) => res.rows[0])
+    .catch((err) => console.log(err));
+};
+exports.getUserInfo = getUserInfo;
+
+/**
+   * Add user to database with given information
+   * @param {Object} options
+**/
+const addAccountToOrg = (options, orgId) => {
+  const query = `
+  INSERT INTO accounts (name, password, website, account_type_id, org_id, creation_date)
+  VALUES ($1, $2, $3, $3, $4, $5, $6)
+  RETURNING *;`
+  return db.query(query, [userId])
+    .then((res) => res.rows[0])
     .catch((err) => console.log(err));
 };
 exports.getUserInfo = getUserInfo;
