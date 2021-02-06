d5668fc1a204282050bc3955dac8cc26e954a0a1
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 15:11:39 +0000

    update routes

--- server.js
+++ server.js
@@ -34,12 +34,14 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const registrationRoutes = require("./routes/registration");
+const apiRoutes = require("./routes/api");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/registration", registrationRoutes(db));
+app.use("/api", apiRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
