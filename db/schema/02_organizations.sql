b2e755e261cb56f6143d697a7b86919c1bcf0689
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 00:02:00 +0000

    create table for organizations

---
+++ db/schema/02_organizations.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS organizations CASCADE;
+
+CREATE TABLE organizations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  abbreviation VARCHAR(3) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  identifier_key VARCHAR(255) NOT NULL,
+  is_deleted BOOLEAN
+);
