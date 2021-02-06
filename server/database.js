fc322ca2b50784fa634fd69d4e5e91d300c5edce
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 21:25:40 +0000

    update to store password as hashed

--- server/database.js
+++ server/database.js
@@ -126,13 +126,14 @@ exports.updateUserInfo = updateUserInfo;
    * @param {Object} params contains all account info including org_id
 **/
 const addAccountToOrg = (params) => {
+  const hashedPassword = bcrypt.hashSync(params.password, 12);
   const query = `
   INSERT INTO accounts (name, password, website, account_type_id, org_id, creation_date)
   VALUES ($1, $2, $3, $4, $5, NOW()::timestamp)
   RETURNING *;`
   return db.query(query, [
     params.name,
-    params.password,
+    hashedPassword,
     params.website,
     params.account_type_id,
     params.org_id
