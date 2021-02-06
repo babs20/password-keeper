530a783314351fd6fbb11ca5230465343d28b4ba
Brady <thevillemc@gmail.com>
2021-02-06 23:50:46 +0000

    create ajax requests for all routes

---
+++ public/scripts/network.js
@@ -0,0 +1,152 @@
+const getHomepage = function() {
+  return $.ajax({
+    url: '/',
+  });
+};
+
+const userLogin = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/users/login',
+    data
+  });
+};
+
+const userLogout = function() {
+  return $.ajax({
+    method: 'POST',
+    url: '/users/logout'
+  });
+};
+
+const userRegistration = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/users/registration',
+    data
+  });
+};
+
+const orgLogin = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/organizations/login',
+    data
+  });
+};
+
+const orgLogout = function() {
+  return $.ajax({
+    method: 'POST',
+    url: '/organizations/logout'
+  });
+};
+
+const orgRegistration = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/organizations/registration',
+    data
+  });
+};
+
+const getUserInfo = function() {
+  return $.ajax({
+    url: '/api/user'
+  });
+};
+
+const editUserInfo = function(data) {
+  return $.ajax({
+    method: 'PUT',
+    url: '/api/user',
+    data
+  });
+};
+
+const deleteUser = function() {
+  return $.ajax({
+    method: 'DELETE',
+    url: '/api/user'
+  });
+};
+
+const createAccount = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/accounts',
+    data
+  });
+};
+
+const getAllAccounts = function(params) {
+  let url = '/api/properties';
+  if (params) {
+    url += '?' + params;
+  }
+  return $.ajax({
+    url
+  });
+};
+
+const editAccount = function(data) {
+  return $.ajax({
+    method: 'PUT',
+    url: '/api/accounts',
+    data
+  });
+};
+
+const deleteAccount = function() {
+  return $.ajax({
+    method: 'DELETE',
+    url: '/api/accounts'
+  });
+};
+
+const getGeneratePassword = function() {
+  return $.ajax({
+    url: '/api/generate-password'
+  });
+};
+
+const generatePassword = function(data) {
+  return $.ajax({
+    method: 'POST',
+    url: '/api/generate-password'
+  });
+};
+
+const getOrgInfo = function() {
+  return $.ajax({
+    url: '/api/organization'
+  });
+};
+
+const editOrgInfo = function(data) {
+  return $.ajax({
+    method: 'PUT',
+    url: '/api/organization',
+    data
+  });
+};
+
+const deleteOrg = function() {
+  return $.ajax({
+    method: 'DELETE',
+    url: '/api/organization'
+  });
+};
+
+const getUsersInOrg = function() {
+  return $.ajax({
+    url: '/api/manage'
+  });
+};
+
+const removeUserFromOrg = function() {
+  return $.ajax({
+    method: 'DELETE',
+    url: '/api/manage'
+  });
+};
