aadd4dc17cd95a5a18eae81a9dab5bf85426de21
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 15:47:28 +0000

    refactor to connect to pool in index

--- server/index.js
+++ server/index.js
@@ -2,13 +2,10 @@ const { Pool } = require('pg');
 const dbParams = require('../lib/db');
 
 const pool = new Pool(dbParams);
+pool.connect();
 
 module.exports = {
   query: (text, params) => {
     return pool.query(text, params);
   },
-
-  connect: () => {
-    return pool.connect();
-  }
 };
