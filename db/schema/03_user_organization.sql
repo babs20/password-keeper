2dec8bdb5090ffbee3438eda5b248b68522e9f8a
Brady <thevillemc@gmail.com>
2021-02-06 00:22:53 +0000

    refactor db schema

--- db/schema/03_user_organization.sql
+++ db/schema/03_user_organization.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS users_organizations CASCADE;
 CREATE TABLE users_organizations (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  org_id INTEGER REFERENCES organization(id) ON DELETE CASCADE,
+  org_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
   is_deleted BOOLEAN DEFAULT FALSE
 );
