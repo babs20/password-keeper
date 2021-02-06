fb546dd9b0964b003897ba5fbc1d51bb6132f2f6
Rebecca Chen <rebecca061996@gmail.com>
2021-02-06 15:49:11 +0000

    create functions to add a user

--- server/database.js
+++ server/database.js
@@ -7,3 +7,26 @@ const getUserWithEmail = function(email) {
   `, [email])
     .then(res => res.rows[0]);
 };
+exports.getUserWithEmail = getUserWithEmail;
+
+const addUser = (user) => {
+  const query = `
+  INSERT INTO organizations (name, abbreviation, email, password, identifier_key)
+  VALUES ($1, $2, $3, $3, $4)
+  RETURNING *;`
+  return db.query(query, [user.name, user.abbr])
+    .then((res) => res)
+    .catch();
+};
+exports.addUser = addUser;
+
+const getUserInfo = (user) => {
+  const query = `
+  INSERT INTO organizations (name, abbreviation, email, password, identifier_key)
+  VALUES ($1, $2, $3, $3, $4)
+  RETURNING *;`
+  return db.query(query, [user.name, user.abbr])
+    .then((res) => res)
+    .catch();
+};
+exports.addUser = addUser;
