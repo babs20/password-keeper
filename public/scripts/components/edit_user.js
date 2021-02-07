644d31848386c6fc900da64201c00abed0d961c0
Brady <thevillemc@gmail.com>
2021-02-07 19:33:52 +0000

    change the way the forms are handled so they aren't nested

--- public/scripts/components/edit_user.js
+++ public/scripts/components/edit_user.js
@@ -1,6 +1,7 @@
 $(() => {
 
   const $editUserForm = $(`
+  <form id="generate-password-form">/<form>
   <div class="w-screen h-100 flex flex-col items-center justify-center mb-10">
   <form id="edit-user-form" class="edit-user-form w-7/12 h-100 flex flex-col items-center justify-center">
     <h4 class="edit-user font-sans text-2xl font-bold w-2/3 my-5">Edit Account Information</h4>
@@ -8,25 +9,25 @@ $(() => {
     <div class="first-last-name flex items-center justify-between mb-3 w-2/3">
       <div class="edit-user-form_field-wrapper flex flex-col w-9/20">
         <label for="first_name" class="label">First Name</label>
-        <input type="text" name="first_name" placeholder="First Name" class="input">
+        <input type="text" name="first_name" placeholder="First Name" class="input" form="edit-user-form">
       </div>
 
       <div class="edit-user-form_field-wrapper flex flex-col w-9/20">
         <label for="last_name" class="label">Last Name</label>
-        <input type="text" name="last_name" placeholder="Last Name" class="input">
+        <input type="text" name="last_name" placeholder="Last Name" class="input" form="edit-user-form">
       </div>
     </div>
 
     <div class="edit-user-form_field-wrapper form-field">
       <label for="email" class="label">Email</label>
-      <input type="email" name="email" placeholder="Email" class="input">
+      <input type="email" name="email" placeholder="Email" class="input" form="edit-user-form">
     </div>
 
     <div class="password-with-generator flex items-center justify-between mb-3 w-2/3">
       <div class="edit-user-form_field-wrapper flex flex-col w-full">
         <label for="password" class="label">Password</label>
         <div class="flex justify-between w-full">
-          <input type="password" name="password" placeholder="Password" class="input w-3/4 mr-3" id="user-password-field">
+          <input type="password" name="password" placeholder="Password" class="input w-3/4 mr-3" id="user-password-field" form="edit-user-form">
           <button class="generate-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5">Generate</button>
         </div>
       </div>
@@ -38,41 +39,39 @@ $(() => {
     </div>
 
     <div class="edit-user-form_field-wrapper form-field">
-      <button class="save-user-info rounded p-1 bg-button w-full text-white hover:bg-hoverBlue mt-1.5">Save Account</button>
+      <button class="save-user-info rounded p-1 bg-button w-full text-white hover:bg-hoverBlue mt-1.5" form="edit-user-form">Save Account</button>
     </div>
 
     <div class="edit-user-form_field-wrapper flex justify-between w-2/3">
       <button class="cancel rounded p-1 bg-button w-2/3 text-white hover:bg-hoverBlue mt-1.5 mr-3">Cancel</button>
       <button class="delete-user rounded p-1 bg-warning w-1/3 text-white hover:bg-warningHover mt-1.5">Delete</button>
     </div>
-
   </form>
-</div>
+ </div>
   `);
 
   window.$editUserForm = $editUserForm;
 
   const $generatePassword = `
-    <form id="generate-password-form">
+
       <div class="password-generator_field-wrapper">
         <label for="length">Password Length</label>
-        <input type="number" name="length" class="password-generator" placeholder="length" id="password-option0">
+        <input type="number" name="length" class="password-generator" placeholder="length" id="password-option0" form="generate-password-form">
       </div>
 
       <div class="password-generator_field-wrapper">
-        <input type="checkbox" id="password-option1" name="lc" value="lower-case" class="password-option">
+        <input type="checkbox" id="password-option1" name="lc" value="lower-case" class="password-option" form="generate-password-form">
         <label for="password-option1"> Lower Case</label><br>
 
-        <input type="checkbox" id="password-option2" name="uc" value="upper-case" class="password-option">
+        <input type="checkbox" id="password-option2" name="uc" value="upper-case" class="password-option" form="generate-password-form">
         <label for="password-option2"> Upper Case</label><br>
 
-        <input type="checkbox" id="password-option3" name="num" value="numbers" class="password-option">
+        <input type="checkbox" id="password-option3" name="num" value="numbers" class="password-option" form="generate-password-form">
         <label for="password-option3"> Numbers</label><br>
 
-        <input type="checkbox" id="password-option4" name="sym" value="symbols" class="password-option">
+        <input type="checkbox" id="password-option4" name="sym" value="symbols" class="password-option" form="generate-password-form">
         <label for="password-option4"> Symbols</label><br>
       </div>
-    </form>
   `;
 
   const $passwordField = $('.password-with-generator');
