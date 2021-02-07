3e44609b0cd321771c8c18f8b0ba6e1725f5b8ce
Brady <thevillemc@gmail.com>
2021-02-07 22:04:17 +0000

    add event listeners for add account page

--- public/scripts/components/add_account.js
+++ public/scripts/components/add_account.js
@@ -1,6 +1,7 @@
 $(() => {
 
   const $addAccountForm = `
+    <form id="generate-password-form">/<form>
     <form id="add-account-form">
       <h4 class="add-account>Add account login information</h4>
 
@@ -40,5 +41,63 @@ $(() => {
   `;
 
   window.$addAccountForm = $addAccountForm;
+
+  const $generatePassword = `
+    <div class="password-generator_field-wrapper">
+      <label for="length">Password Length</label>
+      <input type="number" name="length" class="password-generator" placeholder="length" id="password-option0" form="generate-password-form">
+    </div>
+
+    <div class="password-generator_field-wrapper">
+      <input type="checkbox" id="password-option1" name="lc" value="lower-case" class="password-option" form="generate-password-form">
+      <label for="password-option1"> Lower Case</label><br>
+
+      <input type="checkbox" id="password-option2" name="uc" value="upper-case" class="password-option" form="generate-password-form">
+      <label for="password-option2"> Upper Case</label><br>
+
+      <input type="checkbox" id="password-option3" name="num" value="numbers" class="password-option" form="generate-password-form">
+      <label for="password-option3"> Numbers</label><br>
+
+      <input type="checkbox" id="password-option4" name="sym" value="symbols" class="password-option" form="generate-password-form">
+      <label for="password-option4"> Symbols</label><br>
+    </div>
+  `;
+
+  const $passwordField = $('.password-with-generator');
+
+  $('.generate-password').on('click', function(event) {
+    $generatePassword.appendTo($passwordField);
+
+    $('#password-option0').on('input', function(event) {
+      const data = $('#generate-password-form').serialize();
+      generatePassword(data)
+        .then(password => {
+          $('#user-password-field').val(password);
+          $('#confirm-user-password').val(password);
+        })
+        .catch(e => console.log(e));
+    });
+
+    $('.password-option').on('click', function(event) {
+      const data = $('#generate-password-form').serialize();
+      generatePassword(data)
+        .then(password => {
+          $('#user-password-field').val(password);
+          $('#confirm-user-password').val(password);
+        })
+        .catch(e => console.log(e));
+    });
+
+  });
+
+  $('#add-account-button').on('submit', function(event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    createAccount(data)
+      .then(() => {
+        views_manager.show('allAccounts');
+      });
+  });
 
 });
