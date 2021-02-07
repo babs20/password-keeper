3c7d67e6d565c6817effe81e2672df268cfed200
Brady <thevillemc@gmail.com>
2021-02-07 04:32:24 +0000

    add styles for organization login:

--- public/scripts/components/org_login_form.js
+++ public/scripts/components/org_login_form.js
@@ -1,24 +1,29 @@
 $(() => {
 
   const $orgLoginForm = $(`
-    <form id="org-login-form" class="org-login-form">
-      <p>Organization Login</p>
-      <div class="login-form_field-wrapper">
-        <label for="email">Email</label>
-        <input type="email" name="email" placeholder="email">
-      </div>
+  <div class="login-form w-screen h-100 flex flex-col items-center justify-center mb-10">
+  <form id="user-login-form" class="user-login-form h-72 w-80 flex flex-col items-center justify-center">
+    <p class="font-sans text-2xl font-bold w-2/3 mb-1">Organization Login</p>
+    <div class="login-form_field-wrapper flex flex-col m-3 w-2/3">
+      <label for="email" class="font-bold">Email</label>
+      <input type="email" name="email" placeholder="email" class="mt-2 border-2 rounded border-gray-400 p-1">
+    </div>
 
-      <div class="login-form_field_wrapper">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="password">
-      </div>
+    <div class="login-form_field_wrapper flex flex-col w-2/3 ml-3 mr-3 mb-3">
+      <label for="password" class="font-bold">Password</label>
+      <input type="password" name="password" placeholder="password" class="mt-2 border-2 rounded border-gray-400 p-1">
+    </div>
 
-      <div class="login-form_field_wrapper">
-        <button>Login</button>
-      </div>
-    </form>
-    <p>Trying to login as a user? <a id="user-login-link" href="/users/login">Login Here</a>
-    <p>Don't have an organization account? <a id="org-sign-up-link" href="/organizations/registration">Sign Up</a>
+    <div class="login-form_field_wrapper w-2/3">
+      <button class="mt-2 rounded p-1 bg-button w-full text-white hover:bg-hoverBlue">Login</button>
+    </div>
+  </form>
+  <div class="flex flex-col items-center justify-center w-72 mt-2">
+    <p class="text-l font-normal">User? <button id="org-login-link" class="cursor-pointer text-button hover:underline">Login</button></p>
+
+    <p class="text-l font-normal">Don't have an account? <button id="sign-up-link"  class="cursor-pointer text-button hover:underline">Register</button></p>
+  </div>
+</div>
   `);
 
   window.$orgLoginForm = $orgLoginForm;
