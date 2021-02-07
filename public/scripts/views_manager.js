ca113bf455bc38b5e59d0f6b4aef05de036dec07
Brady <thevillemc@gmail.com>
2021-02-07 22:46:32 +0000

    move event listeners back to their respective files

--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -21,52 +21,15 @@ $(() => {
         break;
       case 'login':
         $loginForm.appendTo($main);
-        $('#user-login-form').on('submit', function(event) {
-          event.preventDefault();
-
-          const data = $(this).serialize();
-          userLogin(data)
-            .then(getUserInfo)
-            .then(json => {
-              header.update(json.user);
-              views_manager.show('allAccounts');
-            });
-
-        });
-        $('#org-login-link').on('click', (event) => {
-          views_manager.show('orgLogin');
-        });
-        $('#sign-up-link').on('click', (event) => {
-          views_manager.show('signup');
-        });
         break;
       case 'orgLogin':
         $orgLoginForm.appendTo($main);
-
-        $('#org-login-form').on('submit', function(event) {
-          event.preventDefault();
-
-          const data = $(this).serialize();
-          orgLogin(data)
-            .then(getOrgInfo)
-            .then(json => {
-              header.update(json.org);
-              views_manager.show('allAccounts');
-            });
-
-        });
-        $('#user-login-link').on('click', (event) => {
-          views_manager.show('login');
-        });
-        $('#org-sign-up-link').on('click', (event) => {
-          views_manager.show('orgSignup');
-        });
         break;
       case 'orgSignup':
         $orgSignupForm.appendTo($main);
         break;
       case 'allAccounts':
-        $homepage.appendTo($main);
+        $editUserForm.appendTo($main);
         break;
       case 'editUser':
         $editUserForm.appendTo($main);
