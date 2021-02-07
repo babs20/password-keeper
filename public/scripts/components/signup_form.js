148780a2a9d93e043d03d9727b5e3029f92dd905
Brady <thevillemc@gmail.com>
2021-02-07 04:33:10 +0000

    add styles for user registration

--- public/scripts/components/signup_form.js
+++ public/scripts/components/signup_form.js
@@ -1,39 +1,41 @@
 $(() => {
 
   const $signupForm = $(`
-    <form id="signup-form" class="signup-form">
-      <p>Sign up for a free account</p>
+  <div class="w-screen h-100 flex flex-col items-center justify-center mb-10">
+  <form id="signup-form" class="signup-form w-7/12 h-100 flex flex-col items-center justify-center">
+    <p class="font-sans text-2xl font-bold w-2/3 my-5">Sign up for a free account</p>
 
-      <div class="first-last-name">
-        <div class="signup-form_field-wrapper">
-          <label for="first_name">First Name</label>
-          <input type="text" name="first_name" placeholder="First Name">
-        </div>
-
-        <div class="signup-form_field-wrapper">
-          <label for="last_name">Last Name</label>
-          <input type="text" name="last_name" placeholder="Last Name">
-        </div>
+    <div class="first-last-name flex items-center justify-between mb-3 w-2/3">
+      <div class="signup-form_field-wrapper flex flex-col w-9/20">
+        <label for="first_name" class="label">First Name</label>
+        <input type="text" name="first_name" placeholder="First Name" class="input">
       </div>
 
-      <div class="signup-form_field-wrapper">
-        <label for="email">Email</label>
-        <input type="email" name="email" placeholder="Email">
+      <div class="signup-form_field-wrapper flex flex-col w-9/20">
+        <label for="last_name" class="label">Last Name</label>
+        <input type="text" name="last_name" placeholder="Last Name" class="input">
       </div>
+    </div>
 
-      <div class="signup-form_field-wrapper">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Password">
-      </div>
+    <div class="signup-form_field-wrapper form-field">
+      <label for="email" class="label">Email</label>
+      <input type="email" name="email" placeholder="Email" class="input">
+    </div>
 
-      <div class="signup-form_field-wrapper">
-        <label for="org_id">Organization ID</label>
-        <input type="text" name="org_id" placeholder="Organization ID">
-      </div>
+    <div class="signup-form_field-wrapper form-field">
+      <label for="password" class="label">Password</label>
+      <input type="password" name="password" placeholder="Password" class="input">
+    </div>
 
-      <div class="signup-form_field_wrapper">
-        <button>Register</button>
-      </div>
+    <div class="signup-form_field-wrapper form-field">
+      <label for="org_id" class="label">Organization ID</label>
+      <input type="text" name="org_id" placeholder="Organization ID"  class="input">
+    </div>
+
+    <div class="signup-form_field_wrapper form-field">
+      <button class="button">Register</button>
+    </div>
+</div>
   `);
 
   window.$signupForm = $signupForm;
