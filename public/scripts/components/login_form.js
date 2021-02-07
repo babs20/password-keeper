9b1509e5f3b233e2816838a6a67342074e13b01a
Brady <thevillemc@gmail.com>
2021-02-07 03:01:55 +0000

    add login form to window, create event listener for submit, move event listeners for links to views manager

--- public/scripts/components/login_form.js
+++ public/scripts/components/login_form.js
@@ -1,25 +1,32 @@
 $(() => {
 
   const $loginForm = $(`
-    <form id="user-login-form" class="user-login-form">
-      <p>Login</p>
-      <div class="login-form_field-wrapper">
-        <label for="email">Email</label>
-        <input type="email" name="email" placeholder="email">
-      </div>
+  <div class="login-form w-screen h-100 flex flex-col items-center justify-center mb-10">
+  <form id="user-login-form" class="user-login-form h-72 w-80 flex flex-col items-center justify-center">
+    <p class="font-sans text-2xl font-bold w-2/3 mb-1">User Login</p>
+    <div class="login-form_field-wrapper flex flex-col m-3 w-2/3">
+      <label for="email" class="text-l font-bold">Email</label>
+      <input type="email" name="email" placeholder="email" class="mt-2 border-2 rounded border-gray-400 p-1">
+    </div>
 
-      <div class="login-form_field_wrapper">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="password">
-      </div>
+    <div class="login-form_field_wrapper flex flex-col w-2/3 ml-3 mr-3 mb-3">
+      <label for="password" class="text-l font-bold">Password</label>
+      <input type="password" name="password" placeholder="password" class="mt-2 border-2 rounded border-gray-400 p-1">
+    </div>
 
-      <div class="login-form_field_wrapper">
-        <button>Login</button>
-      </div>
-    </form>
-    <p>Trying to login as an organization? <a id="org-login-link" href="/organizations/login">Login Here</a>
-    <p>Don't have an account? <a id="sign-up-link" href="/users/registration">Sign Up</a>
+    <div class="login-form_field_wrapper w-2/3">
+      <button class="mt-2  rounded p-1 bg-button w-full text-white hover:bg-hoverBlue">Login</button>
+    </div>
+  </form>
+  <div class="flex flex-col items-center justify-center w-72 mt-2">
+    <p class="text-l font-normal">Organization?</p>
+    <button id="org-login-link" class="cursor-pointer text-button hover:underline">Login</button>
+    <p class="text-l font-normal">Don't have an account? <button id="sign-up-link"  class="cursor-pointer text-button hover:underline">Register</button></p>
+  </div>
+</div>
   `);
+
+  window.$loginForm = $loginForm;
 
   $('#user-login-form').on('submit', function(event) {
     event.preventDefault();
@@ -28,18 +35,10 @@ $(() => {
     userLogin(data)
       .then(getUserInfo)
       .then(json => {
-        updateHeader(json.user);
+        header.update(json.user);
         // views_manager.show
       });
 
   });
-
-  $('#org-login-link').on('click', (event) => {
-    views_manager.show('orgLogin');
-  });
-
-  $('#sign-up-link').on('click', (event) => {
-    views_manager.show('signup');
-  })
 
 });
