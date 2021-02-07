b2b1ba9b2c0ced29f3a350514781f78f59ce6ef8
Brady <thevillemc@gmail.com>
2021-02-07 02:15:24 +0000

    create views manager and add components for login, signup, homepage

--- public/scripts/views_manager.js
+++ public/scripts/views_manager.js
@@ -4,8 +4,23 @@ $(() => {
   window.views_manager = {};
 
   window.views_manager.show = function(item) {
+    $homepage.detach();
+    $loginForm.detach();
+    $signupForm.detach();
+    $orgLoginForm.detach();
 
-  }
+    switch (item) {
+      case 'signup':
+        $signupForm.appendTo($main);
+        break;
+      case 'login':
+        $loginForm.appendTo($main);
+        break;
+      case 'orgLogin':
+        $orgLoginForm.appendTo($main);
+        break;
+    }
+  };
 
 
 
