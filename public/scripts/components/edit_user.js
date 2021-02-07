8ce16958927450a409d4c1d8e2a32a815edb6f9f
Brady <thevillemc@gmail.com>
2021-02-07 18:30:34 +0000

    add handling for save, cancel, and delete user

--- public/scripts/components/edit_user.js
+++ public/scripts/components/edit_user.js
@@ -28,8 +28,13 @@ $(() => {
         </div>
 
         <div class="edit-user-form_field-wrapper">
-          <button class="generate-password">Generate</button>
+          <button type="button" class="generate-password">Generate</button>
         </div>
+      </div>
+
+      <div class="edit-user-form_field-wrapper">
+        <label for="confirm-password">Confirm Password</label>
+        <input type="password" name="confirm-password" placeholder="Password>
       </div>
 
       <div class="edit-user-form_field-wrapper">
@@ -37,15 +42,43 @@ $(() => {
       </div>
 
       <div class="edit-user-form_field-wrapper">
-        <button class="cancel">Cancel</button>
+        <button type="button" class="cancel">Cancel</button>
       </div>
 
       <div class="edit-user-form_field-wrapper">
-        <button class="delete-user">Delete Account</button>
+        <button type="button" class="delete-user">Delete Account</button>
       </div>
     </form>
   `);
 
   window.$editUserForm = $editUserForm;
+
+  $('.generate-password').on('click', function(event) {
+    // append to page the password generator form
+  });
+
+  $('.save-user-info').on('submit', function(event) {
+    event.preventDefault();
+
+    const data = $(this).serialize();
+    editUserInfo(data)
+      .then(() => {
+        views_manager.show('allAccounts');
+      });
+  });
+
+  $('.cancel').on('click', function(event) {
+    views_manager.show('allAccounts');
+  });
+
+  $('.delete-user').on('click', function(event) {
+    deleteUser()
+      .then(userLogout)
+      .then(getUserInfo)
+      .then(json => {
+        header.update(json.user);
+        views_manager.show('homepage');
+      });
+  });
 
 });
