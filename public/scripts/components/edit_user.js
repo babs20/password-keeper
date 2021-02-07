38d0bd76869f84800d1f425544be80f592c226d2
Brady <thevillemc@gmail.com>
2021-02-07 19:01:08 +0000

    add event listener for generate password button to append password options on click

--- public/scripts/components/edit_user.js
+++ public/scripts/components/edit_user.js
@@ -1,60 +1,100 @@
 $(() => {
 
   const $editUserForm = $(`
-    <form id="edit-user-form" class="edit-user-form">
-      <h4 class="edit-user">Edit Account Information</h4>
+  <div class="w-screen h-100 flex flex-col items-center justify-center mb-10">
+  <form id="edit-user-form" class="edit-user-form w-7/12 h-100 flex flex-col items-center justify-center">
+    <h4 class="edit-user font-sans text-2xl font-bold w-2/3 my-5">Edit Account Information</h4>
 
-      <div class="first-last-name">
-        <div class="edit-user-form_field-wrapper">
-          <label for="first_name">First Name</label>
-          <input type="text" name="first_name" placeholder="First Name">
+    <div class="first-last-name flex items-center justify-between mb-3 w-2/3">
+      <div class="edit-user-form_field-wrapper flex flex-col w-9/20">
+        <label for="first_name" class="label">First Name</label>
+        <input type="text" name="first_name" placeholder="First Name" class="input">
+      </div>
+
+      <div class="edit-user-form_field-wrapper flex flex-col w-9/20">
+        <label for="last_name" class="label">Last Name</label>
+        <input type="text" name="last_name" placeholder="Last Name" class="input">
+      </div>
+    </div>
+
+    <div class="edit-user-form_field-wrapper form-field">
+      <label for="email" class="label">Email</label>
+      <input type="email" name="email" placeholder="Email" class="input">
+    </div>
+
+    <div class="password-with-generator flex items-center justify-between mb-3 w-2/3">
+      <div class="edit-user-form_field-wrapper flex flex-col w-full">
+        <label for="password" class="label">Password</label>
+        <div class="flex justify-between w-full">
+          <input type="password" name="password" placeholder="Password" class="input w-3/4 mr-3" id="user-password-field">
+          <button class="generate-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5">Generate</button>
         </div>
-
-        <div class="edit-user-form_field-wrapper">
-          <label for="last_name">Last Name</label>
-          <input type="text" name="last_name" placeholder="Last Name">
-        </div>
       </div>
+    </div>
 
-      <div class="edit-user-form_field-wrapper">
-        <label for="email">Email</label>
-        <input type="email" name="email" placeholder="Email">
-      </div>
+    <div class="edit-user-form_field-wrapper form-field">
+      <label for="confirm-password" class="label">Confirm Password</label>
+      <input type="password" name="confirm-password" placeholder="Confirm Password" class="input" id="confirm-user-password">
+    </div>
 
-      <div class="password-with-generator">
-        <div class="edit-user-form_field-wrapper">
-          <label for="password">Password</label>
-          <input type="password" name="password" placeholder="Password">
-        </div>
+    <div class="edit-user-form_field-wrapper form-field">
+      <button class="save-user-info rounded p-1 bg-button w-full text-white hover:bg-hoverBlue mt-1.5">Save Account</button>
+    </div>
 
-        <div class="edit-user-form_field-wrapper">
-          <button type="button" class="generate-password">Generate</button>
-        </div>
-      </div>
+    <div class="edit-user-form_field-wrapper flex justify-between w-2/3">
+      <button class="cancel rounded p-1 bg-button w-2/3 text-white hover:bg-hoverBlue mt-1.5 mr-3">Cancel</button>
+      <button class="delete-user rounded p-1 bg-warning w-1/3 text-white hover:bg-warningHover mt-1.5">Delete</button>
+    </div>
 
-      <div class="edit-user-form_field-wrapper">
-        <label for="confirm-password">Confirm Password</label>
-        <input type="password" name="confirm-password" placeholder="Password>
-      </div>
-
-      <div class="edit-user-form_field-wrapper">
-        <button class="save-user-info">Save</button>
-      </div>
-
-      <div class="edit-user-form_field-wrapper">
-        <button type="button" class="cancel">Cancel</button>
-      </div>
-
-      <div class="edit-user-form_field-wrapper">
-        <button type="button" class="delete-user">Delete Account</button>
-      </div>
-    </form>
+  </form>
+</div>
   `);
 
   window.$editUserForm = $editUserForm;
 
+  const $generatePassword = `
+    <form id="generate-password-form">
+      <div class="password-generator_field-wrapper">
+        <label for="length">Password Length</label>
+        <input type="number" name="length" class="password-generator" placeholder="length">
+      </div>
+
+      <div class="password-generator_field-wrapper">
+        <input type="checkbox" id="password-option1" name="lc" value="lower-case">
+        <label for="password-option1"> Lower Case</label><br>
+
+        <input type="checkbox" id="password-option2" name="uc" value="upper-case">
+        <label for="password-option2"> Upper Case</label><br>
+
+        <input type="checkbox" id="password-option3" name="num" value="numbers">
+        <label for="password-option3"> Numbers</label><br>
+
+        <input type="checkbox" id="password-option4" name="sym" value="symbols">
+        <label for="password-option4"> Symbols</label><br>
+      </div>
+
+      <div class="password-generator_field-wrapper">
+        <button class="generate-password-button">Generate Password</button>
+      </div>
+    </form>
+  `;
+
+  const $passwordField = $('.password-with-generator');
+
   $('.generate-password').on('click', function(event) {
-    // append to page the password generator form
+    $generatePassword.appendTo($passwordField);
+
+    $('.generate-password-button').on('submit', function(event) {
+      event.preventDefault();
+
+      const data = $(this).serialize();
+      generatePassword(data)
+        .then(password => {
+          $('#user-password-field').val(password);
+          $('#confirm-user-password').val(password);
+        })
+        .catch(e => console.log(e));
+    });
   });
 
   $('.save-user-info').on('submit', function(event) {
@@ -64,7 +104,8 @@ $(() => {
     editUserInfo(data)
       .then(() => {
         views_manager.show('allAccounts');
-      });
+      })
+      .catch(e => console.log(e));
   });
 
   $('.cancel').on('click', function(event) {
