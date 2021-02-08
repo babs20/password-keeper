d35103f398777ae89660e7084a95fee4782f8f76
Brady <thevillemc@gmail.com>
2021-02-08 17:08:33 +0000

    create edit account form

--- public/scripts/components/edit_account.js
+++ public/scripts/components/edit_account.js
@@ -1,7 +1,95 @@
 $(() => {
   const $editAccountForm = $(`
+  <form id="generate-password-form" class="hidden"></form>
+  <div class="w-screen h-100 flex flex-col items-center justify-center mb-10">
+  <form id="edit-account-form">
+    <h4 class="edit-account>Edit account login information"></h4>
 
+    <div class="edit-account_field-wrapper form-field">
+      <label for="email-username" class="label">Email/Username</label>
+      <input type="text" id="email-username" name="name" placeholder="Login" class="input">
+    </div>
+
+    <div class="password-with-generator flex items-center justify-between mb-3 w-2/3">
+      <div class="edit-account_field-wrapper flex flex-col w-full">
+        <label for="password" class="label">Password</label>
+        <div class="flex justify-between w-full">
+          <input type="password" name="password" placeholder="Password" class="input w-3/4 mr-3" id="account-password-field">
+          <button class="generate-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5">Generate</button>
+        </div>
+      </div>
+    </div>
+
+    <div id="generate-container" class="flex flex-col w-2/3 mb-3 divide-y divide-gray-400 hidden">
+          <div class="flex justify-between items-center divide-x divide-gray-400 pb-2">
+            <div class="flex justify-between items-center w-1/2 pr-3">
+              <label for="length " class="label">Length</label>
+              <input type="number" min="6" max="30" name="length"
+                class="font-sans password-generator mr-4 w-1/3 rounded border-gray-400 border outline-none focus:outline-none text-center bg-white font-semibold text-md hover:text-black focus:text-black"
+                placeholder="length" id="password-option0" value="12" form="generate-password-form">
+            </div>
+            <div class="flex justify-between items-center w-1/2 pl-4">
+              <label for="password-option1" class="label ml-4"> Lower Case</label><br>
+              <input type="checkbox" id="password-option1" name="lc" value="true"
+                class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                form="generate-password-form" checked>
+            </div>
+          </div>
+          <div class="flex justify-between items-center pt-2 divide-x divide-gray-400">
+            <div class="flex justify-between items-center w-full pr-4">
+              <label for="password-option2" class="label"> Upper Case</label><br>
+              <input type="checkbox" id="password-option2" name="uc" value="true"
+                class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                form="generate-password-form">
+            </div>
+            <div class="flex justify-between items-center px-4 w-full">
+              <label for="password-option3" class="label"> Numbers</label><br>
+              <input type="checkbox" id="password-option3" name="num" value="true"
+                class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                form="generate-password-form">
+            </div>
+            <div class="flex justify-between items-center pl-4 w-full">
+              <label for="password-option4" class="label"> Symbols</label><br>
+              <input type="checkbox" id="password-option4" name="sym" value="true"
+                class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                form="generate-password-form">
+            </div>
+          </div>
+        </div>
+
+
+        <div class="edit-account_field-wrapper form-field">
+          <label for="confirm-password" class="label">Confirm Password</label>
+          <input type="password" name="confirm-password" placeholder="Confirm Password" class="input"
+            id="confirm-account-password">
+        </div>
+
+    <div class="edit-account_field-wrapper">
+      <label for="website">Website</label>
+      <input type="url" id="website" name="website" placeholder="website">
+    </div>
+
+    <div class="edit-account_field-wrapper">
+      <label for="account-type">Account Type</label>
+      <select name="account-type" id="account-type-dropdown">
+        <option value="1">Work</option>
+        <option value="2">Entertainment</option>
+        <option value="3">Social</option>
+        <option value="4">Other</option>
+      </select>
+    </div>
+
+    <div class="edit-user-form_field-wrapper flex justify-between w-2/3">
+          <button type="button"
+            class="cancel rounded p-1 bg-button w-2/3 text-white hover:bg-hoverBlue mt-1.5 mr-3">Cancel</button>
+          <button type="button"
+            class="delete-user rounded p-1 bg-warning w-1/3 text-white hover:bg-warningHover mt-1.5">Delete</button>
+        </div>
+  </form>
+  </div>
   `);
 
   window.$editAccountForm = $editAccountForm;
+
+
 });
