818cd31f11f45e3fa5ce34fdcb605c3ea476d0f8
Brady <thevillemc@gmail.com>
2021-02-09 18:49:18 +0000

    update styling for more consistent transitions between each form

--- public/scripts/components/signup_form.js
+++ public/scripts/components/signup_form.js
@@ -1,25 +1,26 @@
 $(() => {
 
   const $signupForm = $(`
-  <div class="w-screen h-100 flex flex-col items-center justify-start mb-10">
-  <form id="signup-form" class="signup-form w-7/12 h-100 flex flex-col items-center justify-center">
-    <p class="font-sans text-2xl font-bold w-2/3 my-5">Sign up for a free account</p>
+  <div class="w-9/12 h-auto flex flex-col items-center justify-start mx-auto mt-10">
+    <div class="w-9/12 flex flex-col items-center h-full pt-16">
+    <form id="signup-form" class="signup-form h-100 w-100 xl:w-3/4 flex flex-col items-center justify-center">
+    <p class="font-sans text-2xl font-bold w-full my-5 border-l-8 border-black pl-4">Sign up for a free account</p>
 
-    <div class="sign-up-blank-error flex flex-col mb-3 w-2/3 hidden bg-alertRed rounded-lg">
+    <div class="sign-up-blank-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
       <h4 class="blank-field-error-message text-white p-2 font-bold text-sm">
         <i class="fas fa-exclamation-triangle px-2"></i>
         Please Complete All Fields
       </h4>
     </div>
 
-    <div class="sign-up-org-error flex flex-col mb-3 w-2/3 hidden bg-alertRed rounded-lg">
+    <div class="sign-up-org-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
       <h4 class="org-error-message text-white p-2 font-bold text-sm">
         <i class="fas fa-exclamation-triangle px-2"></i>
         Incorrect Organization ID
       </h4>
     </div>
 
-    <div class="first-last-name flex items-center justify-between mb-3 w-2/3">
+    <div class="first-last-name flex items-center justify-between mb-3 w-full">
       <div class="signup-form_field-wrapper flex flex-col w-9/20">
         <label for="first_name" class="label">First Name</label>
         <input type="text" name="first_name" placeholder="First Name" class="first-name input">
@@ -31,25 +32,27 @@ $(() => {
       </div>
     </div>
 
-    <div class="signup-form_field-wrapper form-field">
+    <div class="signup-form_field-wrapper flex flex-col w-full ml-3 mr-3 mb-3">
       <label for="email" class="label">Email</label>
       <input type="email" name="email" placeholder="Email" class="signup-email input">
     </div>
 
-    <div class="signup-form_field-wrapper form-field">
+    <div class="signup-form_field-wrapper flex flex-col w-full ml-3 mr-3 mb-3">
       <label for="password" class="label">Password</label>
       <input type="password" name="password" placeholder="Password" class="signup-password input">
     </div>
 
-    <div class="signup-form_field-wrapper form-field">
+    <div class="signup-form_field-wrapper flex flex-col w-full ml-3 mr-3 mb-3">
       <label for="org_id" class="label">Organization ID</label>
       <input type="text" name="org_key" placeholder="Organization ID"  class="org-key input">
     </div>
 
-    <div class="signup-form_field_wrapper form-field">
+    <div class="signup-form_field_wrapper flex flex-col w-full ml-3 mr-3 mb-3">
       <button class="button">Register</button>
     </div>
     </form>
+    <p class="text-l font-normal mt-3">Need an Organization Account? <button type="button" id="org-sign-up-link"  class="cursor-pointer text-button hover:underline">Register</button></p>
+</div>
 </div>
   `);
 
