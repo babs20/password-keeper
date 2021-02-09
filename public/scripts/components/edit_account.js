801923d5c3fcacf46bc639761b74021f41ca088e
Rebecca Chen <rebecca061996@gmail.com>
2021-02-09 03:50:02 +0000

    implement password generate functionality

--- public/scripts/components/edit_account.js
+++ public/scripts/components/edit_account.js
@@ -35,9 +35,9 @@ $(() => {
                 <label for="password" class="label">Password</label>
                 <div class="flex justify-between w-full">
                   <input type="password" name="password" placeholder="Password" class="input w-3/4 mr-3"
-                    id="account-password-field" value=${account.password}>
+                    id="edit-account-password" value=${account.password}>
                   <button
-                    class="generate-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5 text-sm">Generate</button>
+                    type="button" class="generate-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5 text-sm">Generate</button>
                 </div>
               </div>
             </div>
@@ -47,33 +47,33 @@ $(() => {
                 <div class="flex justify-between items-center w-1/2 pr-3">
                   <label for="length " class="label">Length</label>
                   <input type="number" min="6" max="30" name="length"
-                    class="font-sans password-generator mr-4 w-1/3 rounded border-gray-400 border outline-none focus:outline-none text-center bg-white font-semibold text-md hover:text-black focus:text-black"
-                    placeholder="length" id="password-option0" value="12" form="generate-password-form">
+                    class="password-option0 font-sans password-generator mr-4 w-1/3 rounded border-gray-400 border outline-none focus:outline-none text-center bg-white font-semibold text-md hover:text-black focus:text-black"
+                    placeholder="length" value="12" form="generate-password-form">
                 </div>
                 <div class="flex justify-between items-center w-1/2 pl-4">
                   <label for="password-option1" class="label ml-4"> Lower Case</label><br>
-                  <input type="checkbox" id="password-option1" name="lc" value="true"
-                    class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                  <input type="checkbox" name="lc" value="true"
+                    class="password-option1 form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
                     form="generate-password-form" checked>
                 </div>
               </div>
               <div class="flex justify-between items-center pt-2 divide-x divide-gray-400">
                 <div class="flex justify-between items-center w-full pr-4">
                   <label for="password-option2" class="label"> Upper Case</label><br>
-                  <input type="checkbox" id="password-option2" name="uc" value="true"
-                    class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                  <input type="checkbox" name="uc" value="true"
+                    class="password-option2 form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
                     form="generate-password-form">
                 </div>
                 <div class="flex justify-between items-center px-4 w-full">
                   <label for="password-option3" class="label"> Numbers</label><br>
-                  <input type="checkbox" id="password-option3" name="num" value="true"
-                    class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                  <input type="checkbox" name="num" value="true"
+                    class="password-option3 form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
                     form="generate-password-form">
                 </div>
                 <div class="flex justify-between items-center pl-4 w-full">
                   <label for="password-option4" class="label"> Symbols</label><br>
-                  <input type="checkbox" id="password-option4" name="sym" value="true"
-                    class="form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
+                  <input type="checkbox" name="sym" value="true"
+                    class="password-option4 form-checkbox h-3.5 w-3.5 rounded text-button border-gray-400 border ml-3"
                     form="generate-password-form">
                 </div>
               </div>
@@ -83,7 +83,7 @@ $(() => {
             <div class="edit-account_field-wrapper form-field">
               <label for="confirm-password" class="label">Confirm Password</label>
               <input type="password" name="confirm-password" placeholder="Confirm Password" class="input"
-                id="confirm-account-password" value="${account.password}">
+                id="confirm-edit-password" value="${account.password}">
             </div>
 
             <div class="edit-account_field-wrapper form-field">
@@ -168,8 +168,8 @@ $(() => {
     event.preventDefault();
 
     const $emailUsername = $('#email-username').val();
-    const $password = $('#account-password-field').val();
-    const $confirmPassword = $('#confirm-account-password').val();
+    const $password = $('#edit-account-password').val();
+    const $confirmPassword = $('#confirm-edit-password').val();
 
     if ($emailUsername < 1 || $password < 1 || $confirmPassword < 1) {
       $('.empty-fields-error').slideDown(150);
@@ -193,6 +193,26 @@ $(() => {
       .then(() => {
         views_manager.show('allAccounts');
       })
+  });
+
+  $('main').on('input', '.password-option0', function(event) {
+    const data = $('#generate-password-form').serialize();
+    generatePassword(data)
+      .then(password => {
+        $('#edit-account-password').val(password);
+        $('#confirm-edit-password').val(password);
+      })
+      .catch(e => console.log(e));
+  });
+
+  $('main').on('click', '.password-option', function(event) {
+    const data = $('#generate-password-form').serialize();
+    generatePassword(data)
+      .then(password => {
+        $('#edit-account-password').val(password);
+        $('#confirm-edit-password').val(password);
+      })
+      .catch(e => console.log(e));
   });
 
 });
