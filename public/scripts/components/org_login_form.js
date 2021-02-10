8c6d9b8428a62fb793c596050c46a1035ac7996d
Rebecca Chen <rebecca061996@gmail.com>
2021-02-10 18:49:05 +0000

    add blank fields error message and new field for master password

--- public/scripts/components/org_login_form.js
+++ public/scripts/components/org_login_form.js
@@ -13,14 +13,26 @@ $(() => {
           </h4>
         </div>
 
+        <div class="login-blank-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
+          <h4 class="blank-field-error-message text-white p-2 font-bold text-sm">
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Please Fill In All Fields
+          </h4>
+        </div>
+
         <div class="login-form_field-wrapper flex flex-col mt-3 w-full">
           <label for="email" class="font-bold">Email</label>
-          <input type="email" name="email" placeholder="Email" class="input login-email">
+          <input type="email" name="email" placeholder="Email" class="input org-login-email">
         </div>
 
         <div class="login-form_field_wrapper flex flex-col w-full ml-3 mr-3 mb-3">
           <label for="password" class="font-bold">Password</label>
-          <input type="password" name="password" placeholder="Password" class="input login-password">
+          <input type="password" name="password" placeholder="Password" class="input org-login-password">
+        </div>
+
+        <div class="login-form_field_wrapper flex flex-col w-full ml-3 mr-3 mb-3">
+          <label for="master-password" class="font-bold">Password</label>
+          <input type="password" name="master_password" placeholder="Master Password" class="input org-login-master-password">
         </div>
 
         <div class="login-form_field_wrapper w-full">
@@ -42,16 +54,29 @@ $(() => {
   $('main').on('submit', '#org-login-form', function(event) {
     event.preventDefault();
 
+    const $email = ('.org-login-email').val().length;
+    const $password = ('.org-login-password').val().length;
+    const $masterPassword = ('.org-login-master-password').val().length;
+
+    if ($email < 1 || $password < 1 || $masterPassword < 1) {
+      $('.login-error').slideUp(10);
+      $('.login-blank-error').slideDown(200);
+      return;
+    }
+
     const data = $(this).serialize();
     orgLogin(data)
       .then(json => {
         header.update(json.org);
         if (!json.org) {
+          $('.login-blank-error').slideUp(10);
           $('.login-error').slideDown(200);
         } else {
           $('.login-error').slideUp(10);
-          $('.login-email').val('');
-          $('.login-password').val('');
+          $('.login-blank-error').slideUp(10);
+          $('.org-login-email').val('');
+          $('.org-login-password').val('');
+          $('.org-login-master-password').val('');
           sidenav.showSidebar(json.org.id, json.org.user_id)
           .then($sidebar => {
             const $main = $('main');
