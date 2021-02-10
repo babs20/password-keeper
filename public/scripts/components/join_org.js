4e66694dac0e7d47f19e1f0e9480d90a0cb50810
Brady <thevillemc@gmail.com>
2021-02-10 03:39:54 +0000

    add styles to join org form

--- public/scripts/components/join_org.js
+++ public/scripts/components/join_org.js
@@ -1,29 +1,33 @@
 $(() => {
   const $joinOrgForm = $(`
-    <div class="w-10/12 mx-auto h-full flex justify-center">
-      <div class="w-10/12 h-full flex flex-col items-center justify-center">
-        <form id="join-org-form">
-          <h2 class="join-org-title">Join Another Organization</h2>
+    <div class="w-10/12 h-100 mx-auto flex flex-col items-center justify-start">
+        <form id="join-org-form" class="w-7/12 h-100 flex flex-col items-center justify-start mx-auto pt-5">
+          <h1 class="join-org-title font-sans text-2xl font-bold w-full h-full my-5 pl-4 border-l-8 border-black">Join Another Organization</h1>
 
-          <div class="empty-fields-error hidden">
-            <h2 class="empty-fields-message">Organization Id Cannot Be Empty</h2>
+          <div class="empty-fields-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
+            <h2 class="empty-fields-message text-white p-2 font-bold">
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Organization ID Can't Be Empty</h2>
           </div>
 
-          <div class="org-key-error hidden">
-            <h2 class="org-key-message">Organization Does Not Exist</h2>
+          <div class="org-key-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
+            <h2 class="org-key-message text-white p-2 font-bold">
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Organization Does Not Exist</h2>
           </div>
 
-          <div class="org-joined-error hidden">
-            <h2 class="org-joined-message">You Are Already Apart of This Organization</h2>
+          <div class="org-joined-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
+            <h2 class="org-joined-message text-white p-2 font-bold">
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            You Are Already Apart of This Organization</h2>
           </div>
 
-          <div class="join-org_field-wrapper>
+          <div class="join-org_field-wrapper flex flex-col mx-3 mb-3 w-full">
             <label for="org-key" class="label">Organization ID</label>
             <input type="text" id="org-key" name="identifier_key" placeholder="Org ID" class="input">
-            <button id="join-org-button" class="button font-bold">Join Organization</button>
+            <button id="join-org-button" class="mt-2 rounded p-2 bg-button w-full text-white hover:bg-hoverBlue font-bold">Join Organization</button>
           </div>
         </form>
-      </div>
     </div>
   `);
 
