a979a00f56f4be468e22b77cffdcb4a3d1b76544
Brady <thevillemc@gmail.com>
2021-02-11 16:07:26 +0000

    add divides between password sections

--- public/scripts/components/edit_organization.js
+++ public/scripts/components/edit_organization.js
@@ -17,34 +17,43 @@ $(() => {
         $('#edit-org-form').empty();
         const $orgSpecificForm = $(`
         <div class="edit-org-header-section w-full">
-          <h2 class="edit-org font-sans text-2xl font-bold w-full my-5 border-l-8 border-black pl-4">Edit Organization Information</h2>
-          <h3 class="org-identifier-key font-bold rounded border-l-8 p-1 px-4 my-2 bg-white text-black border-gray-400 break-words w-max hover:shadow-md">Organization ID: ${org.key}</h3>
+          <h2 class="edit-org font-sans text-2xl font-bold w-full my-5 border-l-8 border-black pl-4">Edit Organization
+            Information</h2>
+          <h3
+            class="org-identifier-key font-bold rounded border-l-8 p-1 px-4 my-2 bg-white text-black border-gray-400 break-words w-max hover:shadow-md">
+            Organization ID: ${org.key}</h3>
         </div>
 
         <div class="password-match-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg">
           <h2 class="password-match-message text-white p-2 font-bold">
-          <i class="fas fa-exclamation-triangle px-2"></i>
-          Passwords Do Not Match</h2>
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Passwords Do Not Match
+          </h2>
         </div>
 
         <div class="fields-blank-error hidden flex flex-col mb-3 w-full bg-alertRed rounded-lg">
           <h2 class="fields-blank-message text-white p-2 font-bold">
-          <i class="fas fa-exclamation-triangle px-2"></i>
-          Please Fill Out All Fields</h2>
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Please Fill Out All Fields
+          </h2>
         </div>
 
         <div class="master-password-incorrect-error hidden flex flex-col mb-3 w-full bg-alertRed rounded-lg">
           <h2 class="master-password-incorrect-message text-white p-2 font-bold">
-          <i class="fas fa-exclamation-triangle px-2"></i>
-          Current Master Password is Incorrect</h2>
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            Current Master Password is Incorrect
+          </h2>
         </div>
 
         <div class="master-password-match-error hidden flex flex-col mb-3 w-full bg-alertRed rounded-lg">
           <h2 class="master-password-match-message text-white p-2 font-bold">
-          <i class="fas fa-exclamation-triangle px-2"></i>
-          New Master Passwords Do Not Match</h2>
+            <i class="fas fa-exclamation-triangle px-2"></i>
+            New Master Passwords Do Not Match
+          </h2>
         </div>
+        <div>
 
+        </div>
         <div class="name-abbrev flex items-center justify-between mb-3 w-full">
           <div class="edit-org-form_field-wrapper flex flex-col w-9/20">
             <label for="name" class="label">Name</label>
@@ -53,7 +62,8 @@ $(() => {
 
           <div class="edit-org-form_field-wrapper flex flex-col w-9/20">
             <label for="abbreviation" class="label">Abbreviation</label>
-            <input type="text" name="abbreviation" placeholder="Abbreviation" class="org-abbrev input" value="${org.abbreviation}">
+            <input type="text" name="abbreviation" placeholder="Abbreviation" class="org-abbrev input"
+              value="${org.abbreviation}">
           </div>
         </div>
 
@@ -62,20 +72,21 @@ $(() => {
           <input type="email" name="email" placeholder="Email" class="org-email input" value="${org.email}">
         </div>
 
-        <div class="password-with-generator flex items-center justify-between mb-3 w-full">
+        <div class="password-with-generator flex items-center justify-between mb-3 pt-2 w-full border-t-2 border-gray-600">
           <div class="edit-org-form_field-wrapper flex flex-col w-full">
             <label for="password" class="label">Password</label>
             <div id="generator" class="flex justify-between w-full">
-              <input type="password" name="password" placeholder="Password" class="org-password-field mt-2 border-2 rounded border-gray-400 focus:outline-none focus:ring-1 focus:border-button w-3/4 mr-3 pr-16 pl-1 py-1">
+              <input type="password" name="password" placeholder="Password"
+                class="org-password-field mt-2 border-2 rounded border-gray-400 focus:outline-none focus:ring-1 focus:border-button w-3/4 mr-3 pr-16 pl-1 py-1">
 
               <div class="password-buttons flex justify-between items-center w-min ml-2 -ml-20 mr-1 mt-2">
                 <button type="button" id="org-view-password"
-                class="flex justify-center items-center opacity-50 hover:opacity-100"><i title="Show or Hide Password"
-                  class="fas fa-eye px-2"></i></button>
+                  class="flex justify-center items-center opacity-50 hover:opacity-100"><i title="Show or Hide Password"
+                    class="fas fa-eye px-2"></i></button>
                 <button type="button" id="org-copy-password"
-                class="flex justify-center items-center opacity-50 hover:opacity-100"><i title="Copy Password"
-                  class="far fa-clipboard px-2"></i></button>
-                </div>
+                  class="flex justify-center items-center opacity-50 hover:opacity-100"><i title="Copy Password"
+                    class="far fa-clipboard px-2"></i></button>
+              </div>
 
               <button type="button"
                 class="generate-org-password rounded p-1 bg-button w-1/4 text-white hover:bg-hoverBlue mt-1.5 ml-4 font-bold">Generate</button>
@@ -84,10 +95,11 @@ $(() => {
         </div>
 
         <div class="checkbox-error flex flex-col mb-3 w-full hidden bg-alertRed rounded-lg border-none">
-              <h2 class="checkbox-error-message text-white p-2 font-bold">
-              <i class="fas fa-times-circle px-2 text-lg"></i>
-              At Least One Option Must Be Checked</h2>
-            </div>
+          <h2 class="checkbox-error-message text-white p-2 font-bold">
+            <i class="fas fa-times-circle px-2 text-lg"></i>
+            At Least One Option Must Be Checked
+          </h2>
+        </div>
 
         <div id="generate-org-container" class="flex flex-col mb-3 divide-y divide-gray-400 hidden w-full">
           <div class="flex justify-between items-center divide-x divide-gray-400 pb-2">
@@ -128,26 +140,31 @@ $(() => {
 
         <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full">
           <label for="confirm-password" class="label">Confirm Password</label>
-          <input type="password" name="confirm-password" placeholder="Confirm Password" class="confirm-org-password input">
+          <input type="password" name="confirm-password" placeholder="Confirm Password"
+            class="confirm-org-password input">
         </div>
 
-        <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full">
+        <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full pt-2 w-full border-t-2 border-gray-600">
           <label for="current-master-password" class="label">Current Master Password</label>
-          <input type="password" name="current_master_password" placeholder="Master Password" class="current-master-password input">
+          <input type="password" name="current_master_password" placeholder="Master Password"
+            class="current-master-password input">
         </div>
 
         <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full">
           <label for="new-master-password" class="label">New Master Password</label>
-          <input type="password" name="new_master_password" placeholder="New Master Password" class="new-master-password input">
+          <input type="password" name="new_master_password" placeholder="New Master Password"
+            class="new-master-password input">
         </div>
 
         <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full">
           <label for="confirm-master-password" class="label">Confirm Master Password</label>
-          <input type="password" name="confirm-master-password" placeholder="Confirm Master Password" class="confirm-master-password input">
+          <input type="password" name="confirm-master-password" placeholder="Confirm Master Password"
+            class="confirm-master-password input">
         </div>
 
         <div class="edit-org-form_field-wrapper flex flex-col ml-3 mr-3 mb-3 w-full">
-          <button type="submit" class="save-org-info rounded p-1 bg-button w-full text-white hover:bg-hoverBlue mt-1.5 font-bold">Save</button>
+          <button type="submit"
+            class="save-org-info rounded p-1 bg-button w-full text-white hover:bg-hoverBlue mt-1.5 font-bold">Save</button>
         </div>
         <div class="edit-org-form_field-wrapper flex justify-between w-full">
           <button type="button"
