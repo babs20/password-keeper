71cc5b7c94876b14d7df50fc6bae5fd829df42e0
Brady <thevillemc@gmail.com>
2021-02-12 01:15:53 +0000

    fix db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const { Pool } = require('pg');
+const { ssl } = require('../lib/db');
 const dbParams = require('../lib/db');
 
-const pool = new Pool(dbParams);
+const pool = new Pool(
+  {
+    host: 'ec2-52-205-3-3.compute-1.amazonaws.com',
+    database: 'dddae6ankpbtav',
+    user: 'fvwzazmshaoxsj',
+    port: 5432,
+    password: '2369b5bbf5de5b2d1472583cecfc1d8cf204bc66162846ca0296c1b7d2553988',
+    ssl: true,
+  }
+);
 pool.connect();
 
 module.exports = {
